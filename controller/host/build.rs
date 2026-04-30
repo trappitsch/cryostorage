a3a956af5ba55c839f73c552285d8aee3febd7ef
Reto Trappitsch <reto@galactic-forensics.space>
2026-04-30 11:14:30 +0200

    change build_info in executable to time only as jj not available when x compiling

--- controller/host/build.rs
+++ controller/host/build.rs
@@ -14,22 +14,6 @@ fn main() -> io::Result<()> {
 
 fn set_build_version() {
     let build_time = chrono::Local::now().format("%Y-%m-%d %H:%M:%S").to_string();
-
-    let jj_main_hash = Command::new("jj")
-        .args([
-            "log",
-            "--no-graph",
-            "-r",
-            "main",
-            "-T",
-            "self.commit_id().short()",
-        ])
-        .output()
-        .ok()
-        .and_then(|output| String::from_utf8(output.stdout).ok())
-        .map(|s| s.trim().to_string())
-        .unwrap_or("unknown".to_string());
-
-    let build_info = format!("build time: {}, jj main: {}", build_time, jj_main_hash);
+    let build_info = format!("build time: {}", build_time);
     println!("cargo:rustc-env=BUILD_INFO={build_info}");
 }
